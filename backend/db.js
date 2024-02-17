ed30522a0d38ef382fd6c90c3fb0c782dac0755c
Abdul Hafeez <hafizabdulhafeez240302@gmail.com>
2024-02-17 14:20:08 +0530

    connect to database and setup express

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+// mongodb+srv://taskapp:TaskAppApi@cluster0.pj4eq8e.mongodb.net/
+const mongoose = require('mongoose')
+const mongoURI = 'mongodb+srv://taskapp:TaskAppApi@cluster0.pj4eq8e.mongodb.net/'
+
+const connectMongo = () => {
+    mongoose.connect(mongoURI, () => {
+        console.log('Connected to Mongo Successfully!!')
+
+    })
+}
+
+module.exports = connectMongo
