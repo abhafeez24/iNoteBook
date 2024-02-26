38657c6c574ab39f137c550ac52af86ff4ac195b
Abdul Hafeez <hafizabdulhafeez240302@gmail.com>
2024-02-26 16:10:44 +0530

    fix bugs

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
-const mongoURI = 'mongodb+srv://taskapp:TaskAppApi@cluster0.pj4eq8e.mongodb.net/'
+const mongoURI = 'mongodb://127.0.0.1:27017/iNoteBook'
 
-const connectMongo = () => {
+const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
-        console.log('Connected to Mongo Successfully!!')
-
+        console.log('Connected to Mongo Successfully')
     })
 }
 
-module.exports = connectMongo
+
+
+module.exports = connectToMongo
+
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
-const connectMongo = require('./db')
+const connectToMongo = require('./db')
 const express = require('express')
 
-connectMongo();
+connectToMongo();
 
 const app = express()
 const port = 3000
+
+app.use(express.json())
 
 //Available Routes
 app.use('/api/auth', require('./routes/auth'))
