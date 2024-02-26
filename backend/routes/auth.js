c7b7e61e64cd2ed281a9ac6b4e9bb3ef2a48eb81
Abdul Hafeez <hafizabdulhafeez240302@gmail.com>
2024-02-26 17:19:04 +0530

    Hash password and generate token using jsonwebtokens

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express')
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const JWT_SECRET = 'IamaGoodb$oy'
 
 
 //CREATE a User using: POST "/api/auth/createU\user" Doesn't require Login
@@ -25,13 +29,26 @@ router.post('/createuser',[
         if(user) {
             return res.status(400).json({Error: 'Sorry a user with this email exists'})
         }
+
+        //bcrypt password
+        const salt = await bcrypt.genSalt(10)
+        const secPassword = await bcrypt.hash(req.body.password, salt)
+
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: secPassword,
         })
         
-        res.send(req.body)
+        //jwt
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const token = jwt.sign(data, JWT_SECRET)        //generate token synchronously
+        res.json({token})
+
     } catch (error) {
         console.error(error.message)
         res.status(500).send('Some error occured')
